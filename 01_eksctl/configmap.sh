21e764d84c71843a5fd26eafcb229c33d0d5440a
z3r0c00ls <mromano.lazaro@gmail.com>
2024-07-14 19:07:21 -0300

    Limpieza y Orden Repositorio Proyecto PIN_FINAL

--- 01_eksctl/configmap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl edit -n kube-system configmap/aws-auth
-
-#  mapUsers: |
-#    - groups:
-#    - system:masters
-#      userarn:  <arn:aws:iam::xxxx:user/xxx
-#      username: xxx
--- 03_fluentbit/fluentbit-intro.md
+++
@@ -1,3 +0,0 @@
-Para comprender que hace este fluentbiot-deployment.yaml ver este introduccion:
-
-[fluentbit](https://echorand.me/posts/fluentbit-kubernetes/)
